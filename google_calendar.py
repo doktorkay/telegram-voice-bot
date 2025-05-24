681973f6f600990e9756bfc688a685540d3b38f7
doktorkay <kktrone@gmail.com>
2025-05-24 19:41:00 +0200

    Update google_calendar.py

--- google_calendar.py
+++ google_calendar.py
@@ -1,7 +1,6 @@
 import os
 import json
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -9,17 +8,22 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 def create_google_calendar_event(summary, date, start_time, end_time, description=None, location=None, attendees=None):
     creds = None
 
-    # Leggi le credenziali dal segreto
+    # Leggi credentials.json dal segreto
     creds_json = os.environ.get('GOOGLE_CREDENTIALS_JSON')
     if creds_json:
         with open('credentials_temp.json', 'w') as f:
             f.write(creds_json)
-        flow = InstalledAppFlow.from_client_secrets_file('credentials_temp.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
     else:
-        raise Exception("❌ Variabile d'ambiente GOOGLE_CREDENTIALS_JSON mancante")
+        raise Exception("❌ Variabile GOOGLE_CREDENTIALS_JSON mancante")
+
+    # Leggi token.json dal segreto
+    token_json = os.environ.get('GOOGLE_TOKEN_JSON')
+    if token_json:
+        with open('token.json', 'w') as f:
+            f.write(token_json)
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    else:
+        raise Exception("❌ Variabile GOOGLE_TOKEN_JSON mancante")
 
     service = build('calendar', 'v3', credentials=creds)
 
