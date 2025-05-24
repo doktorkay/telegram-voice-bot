b7d95fe7198dae7d5cdb72a6b0b4962ab14ee9af
doktorkay <kktrone@gmail.com>
2025-05-24 19:28:41 +0200

    Update google_calendar.py

--- google_calendar.py
+++ google_calendar.py
@@ -1,4 +1,5 @@
 import os
+import json
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -7,13 +8,18 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def create_google_calendar_event(summary, date, start_time, end_time, description=None, location=None, attendees=None):
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+
+    # Leggi le credenziali dal segreto
+    creds_json = os.environ.get('GOOGLE_CREDENTIALS_JSON')
+    if creds_json:
+        with open('credentials_temp.json', 'w') as f:
+            f.write(creds_json)
+        flow = InstalledAppFlow.from_client_secrets_file('credentials_temp.json', SCOPES)
         creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    else:
+        raise Exception("❌ Variabile d'ambiente GOOGLE_CREDENTIALS_JSON mancante")
 
     service = build('calendar', 'v3', credentials=creds)
 
